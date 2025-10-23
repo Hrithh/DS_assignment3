4a22324586f863b965d8e10952621cc0b40a889f
Harith <a1790387@adelaide.edu.au>
2025-10-23 23:24:26 +1030

    refactor: Align message types with assignment spec
    - Renamed ACCEPT → ACCEPT_REQUEST to match Paxos terminology.
    - Confirmed required types: PREPARE, PROMISE, ACCEPT_REQUEST, ACCEPTED.
    - Retained DECIDE and NACK for robustness and future use.

--- src/main/java/au/edu/adelaide/ds/assignment3/Message.java
+++ src/main/java/au/edu/adelaide/ds/assignment3/Message.java
@@ -1,15 +1,71 @@
 package au.edu.adelaide.ds.assignment3;
 
+/**
+ * Represents a Paxos message that will be serialized and sent between nodes via TCP.
+ */
 public class Message {
-    public String type;
-    public String sender;
-    public String proposalNum;  // Format: "3.1"
-    public String value;
+    public enum MessageType {
+        PREPARE,
+        PROMISE,
+        ACCEPT_REQUEST,
+        ACCEPTED,
+        DECIDE,
+        NACK
+    }
 
-    public Message(String type, String sender, String proposalNum, String value) {
+    private MessageType type;
+    private int proposalNumber;
+    private String value;
+    private String senderId;
+
+    public Message() {}
+
+    public Message(MessageType type, int proposalNumber, String value, String senderId) {
         this.type = type;
-        this.sender = sender;
-        this.proposalNum = proposalNum;
+        this.proposalNumber = proposalNumber;
         this.value = value;
+        this.senderId = senderId;
+    }
+
+    public MessageType getType() {
+        return type;
+    }
+
+    public void setType(MessageType type) {
+        this.type = type;
+    }
+
+    public int getProposalNumber() {
+        return proposalNumber;
+    }
+
+    public void setProposalNumber(int proposalNumber) {
+        this.proposalNumber = proposalNumber;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public String getSenderId() {
+        return senderId;
+    }
+
+    public void setSenderId(String senderId) {
+        this.senderId = senderId;
+    }
+
+    @Override
+    public String toString() {
+        return "Message{" +
+                "type=" + type +
+                ", proposalNumber=" + proposalNumber +
+                ", value='" + value + '\'' +
+                ", senderId='" + senderId + '\'' +
+                '}';
     }
 }
